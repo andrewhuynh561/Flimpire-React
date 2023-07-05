c29a3637461ffc55b89356909eac400dbec1f854
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2023-07-05 18:29:54 +0930

    changed the function

---
+++ src/MovieCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const MovieCard=({movie}) =>{
+
+    return(
+        <div className="movie">
+        <div>
+            <p>{movie.Year}</p>
+        </div>
+
+        <div>
+            <img src={movie.Poster !== 'N/A' ?movie.Poster :'http://via..placholder.com/400'} alt={movie.Title}/>
+        </div>
+
+        <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+
+        </div>
+        
+        <div>
+            <p>{movie.Year}</p>
+        </div>
+
+
+    </div>
+    )
+}
+
+export default MovieCard;
